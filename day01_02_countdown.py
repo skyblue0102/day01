00276613cc5202b396c84336cb40e11f45e6dc44
kangmunju <ikmj0871@gmail.com>
2023-01-12 17:16:00 +0900

    temperature

---
+++ day01_02_countdown.py
@@ -0,0 +1,10 @@
+# 교재 countdown 예제
+# for countdown in 5, 4, 3, 2, 1, "hey!"
+#     print(countdown)
+# print('프로그램 종료')
+
+countdown_list = [5, 4, 3, 2, 1, "hey!"]
+for countdown in countdown_list:
+    print(countdown)
+print(countdown_list[3]) #2
+print(countdown_list[-1]) #2
---
+++ day01_04_gui.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry('400x300')
+win.title('파이썬 1일차 Preview')
+win.mainloop()
